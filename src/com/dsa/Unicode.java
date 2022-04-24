fd5f0d443f7f73ae17e7f37c367fb9622e1a8b35
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-25 00:09:26 +0530

    Add files via upload

---
+++ src/com/dsa/Unicode.java
@@ -0,0 +1,13 @@
+package com.dsa;
+
+public class Unicode {
+    public static void main(String[] args) {
+        // Java follows the unicode principles.
+        // Unicode is a 16-bit character encoding standard and is capable to represent
+        // almost every character of well-known languages of the world.
+        System.out.println("Hello");       // English
+        System.out.println("नमस्ते");        // Hindi
+        System.out.println("你好");        // Chinese
+        System.out.println("こんにちは");   // Japanese
+    }
+}
