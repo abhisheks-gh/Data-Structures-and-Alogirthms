82f175ae061e73e6b16b4bccf17ab29b20d3f277
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-04-26 16:53:28 +0530

    Add files via upload

---
+++ src/com/assignments/Calculator.java
@@ -0,0 +1,31 @@
+// Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Numbers on which the operations are to be done
+        double num1, num2, result;
+        // Operator 
+        char oper;
+        System.out.println("Enter two numbers:");
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+        System.out.println("Enter a operator (+, -, *, /):");
+        // Reading a character
+        oper = sc.next().charAt(0);
+        sc.close();
+
+        if (oper == '+')
+            System.out.println(num1 + num2);
+        if (oper == '-')
+            System.out.println(num1 - num2);
+        if (oper == '*')
+            System.out.println(num1 * num2);
+        if (oper == '/')
+            System.out.println(num1 / num2);
+        if (oper != '+' && oper != '-' && oper != '*' && oper != '/')
+            System.out.println("Operator not in the list! Please try again.");
+    }
+}
