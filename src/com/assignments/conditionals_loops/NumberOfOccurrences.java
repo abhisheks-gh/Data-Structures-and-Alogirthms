9ab366b4c6ddb114ef91485579be3984bc234ed8
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-09 16:51:04 +0530

    Add NumberOfOccurrences.java

---
+++ src/com/assignments/conditionals_loops/NumberOfOccurrences.java
@@ -0,0 +1,15 @@
+// For a given number 1385757879, find the number of occurrences of 7.
+package com.assignments.conditionals_loops;
+
+public class NumberOfOccurrences {
+    public static void main(String[] args) {
+        long num = 1385757879;
+        String str = Long.toString(num);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '7')
+                count++;
+        }
+        System.out.println("Number of occurrences of '7': " + count);
+    }
+}
