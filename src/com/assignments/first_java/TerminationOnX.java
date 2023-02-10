bc64a9438a004b94afa8df8776adda1c8e27b38f
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-10 19:54:45 +0530

    Update TerminationOnX.java

--- src/com/assignments/first_java/TerminationOnX.java
+++ src/com/assignments/first_java/TerminationOnX.java
@@ -1,32 +1,23 @@
+// Keep taking numbers as inputs till the user enters ‘X’ or 'x', after that print sum of all.
+
 package com.assignments.first_java;
 
 import java.util.Scanner;
 
-// Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
-
-import java.util.Scanner;
-// import java.lang.String;
-// import java.lang.Long;
-
 public class TerminationOnX {
     public static void main(String[] args) {
-        // Long sum;
-        // Long userInput;
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter any number:");
-        // while (userInput.toString(userInput) != "x" || userInput.toString(userInput) != "X") {
-        //     userInput = sc.nextLong();
-        //     sum += userInput;
-        // }
-        // sc.close();
-
-        int num = 0, sum = 0;
         Scanner sc = new Scanner(System.in);
-        while (num != 'X') {
-            num = sc.nextInt();
-            sum += num;
+        int sum = 0;
+        while (true) {
+            String input = sc.next().trim();
+            if (input.equals("X") || input.equals("x")) {
+                break;
+            } else {
+                int num = Integer.parseInt(input);
+                sum += num;
+            }
         }
-        System.out.println("Sum = " +  sum);
+        System.out.println(sum);
         sc.close();
     }
 }
