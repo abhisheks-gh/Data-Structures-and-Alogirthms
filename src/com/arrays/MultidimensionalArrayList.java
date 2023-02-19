d099ce716b0e83879cc9a89501759867b73b58c1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-20 03:49:20 +0530

    Add package com.arrays

---
+++ src/com/arrays/MultidimensionalArrayList.java
@@ -0,0 +1,27 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultidimensionalArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // initialization
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add elements
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(sc.nextInt());
+            }
+        }
+
+        System.out.println(list);
+        
+        sc.close();
+    }
+}
---
+++ src/com/arrays/MultidimensionalArrays.java
@@ -0,0 +1,47 @@
+package com.arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        // Specifying the number of columns is not mandatory.
+//        // As 2D-Arrays are array of arrays and these individual arrays
+//        // can be of variable sizes.
+//        int[][] arr = new int[3][];
+//        int[][] arr = {
+//                {1, 2, 3},
+//                {4, 5,},
+//                {6, 7, 8, 9}
+//        };
+
+        int[][] arr = new int[3][3];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = sc.nextInt();
+            }
+        }
+
+//        // Output 2D array
+//        for (int row = 0; row < arr.length; row++ ) {
+//            for (int column = 0; column < arr[row].length; column++) {
+//                System.out.print(arr[row][column] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        // Better way to output 2D array
+//        for (int row = 0; row < arr.length; row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//        }
+
+        // Using enhanced for (for-each) loop to output 2D array
+        for (int[] element : arr) {
+            System.out.println(Arrays.toString(element));
+        }
+        sc.close();
+    }
+}
