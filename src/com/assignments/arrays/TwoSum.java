bfa1bab29b4802750604afd4c8bf27b14370f95d
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-21 15:55:23 +0530

    Update TwoSum.java

--- src/com/assignments/arrays/TwoSum.java
+++ src/com/assignments/arrays/TwoSum.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {3,3};
-        System.out.println( Arrays.toString(twoSum(nums, 6)) );
+        int[] nums = {2, 7, 11, 15};
+        System.out.println( Arrays.toString(twoSum(nums, 9)) );
     }
 
     public static int[] twoSum(int[] nums, int target) {
