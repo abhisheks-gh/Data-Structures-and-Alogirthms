9eda6f5be1693d4dee9f1264a51c033d93edeea3
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-26 09:55:54 +0530

    Add FindMax.java and FindMin.java

---
+++ src/com/searching/FindMax.java
@@ -0,0 +1,25 @@
+package com.searching;
+
+public class FindMax {
+
+    public static void main(String[] args) {
+        int[] arr = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        System.out.println( max(arr) );
+    }
+
+    // returns the maximum value from the array
+    static int max(int[] arr) {
+        if (arr.length == 0)
+            return Integer.MAX_VALUE;
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        return max;
+    }
+
+}
---
+++ src/com/searching/FindMin.java
@@ -0,0 +1,25 @@
+package com.searching;
+
+public class FindMin {
+
+    public static void main(String[] args) {
+        int[] arr = {18, 12,-7, 3, 14, 28};
+        System.out.println( min(arr) );
+    }
+
+    // returns the minimum value from array
+    static int min(int[] arr) {
+        if (arr.length == 0)
+            return Integer.MIN_VALUE;
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        return min;
+    }
+
+}
