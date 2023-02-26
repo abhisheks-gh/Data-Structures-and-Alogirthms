151406f998cfdae285de0d92121397fd62ae9de0
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-02-26 10:09:29 +0530

    Add SearchIn2DArray.java

---
+++ src/com/searching/SearchIn2DArray.java
@@ -0,0 +1,35 @@
+package com.searching;
+
+import java.util.Scanner;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = {
+                {23, 4, 1},
+                {18, 12, 3, 9},
+                {78, 99, 34, 56},
+                {18, 12}
+        };
+
+        System.out.print("Enter the element you want to search: ");
+        int target = sc.nextInt();
+
+        System.out.println( searchIn2DArray(arr, target));
+
+        sc.close();
+    }
+
+    static String searchIn2DArray(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return "Found";
+                }
+            }
+        }
+
+        return "Not found";
+    }
+}
