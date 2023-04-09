56af9f4b2ccdf82400b55237e7b070868f7efab4
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-04-10 03:36:18 +0530

    Update SmallestLetterGreaterThanTarget.java

--- src/com/dsa/searching/binarySearch/interviewQuestions/SmallestLetterGreaterThanTarget.java
+++ src/com/dsa/searching/binarySearch/interviewQuestions/SmallestLetterGreaterThanTarget.java
@@ -2,16 +2,8 @@
 
 package com.dsa.searching.binarySearch.interviewQuestions;
 
-public class SmallestLetterGreaterThanTarget {
-
-    public static void main(String[] args) {
-        char[] letters = {'c', 'f', 'j'};
-        char target = 'a';
-
-        System.out.println( nextGreatestLetter(letters, target) );
-    }
-
-    public static char nextGreatestLetter(char[] letters, char target) {
+class SmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
 
@@ -23,11 +15,10 @@ public class SmallestLetterGreaterThanTarget {
             else
                 start = mid + 1;
         }
-
+        
         // When while loop breaks, start = end + 1 as start will cross end
         // and the while loop breaks when target is not found in the array
         // So, start becomes the element i.e. just greater than the target
         return letters[start % letters.length];
     }
-
 }
