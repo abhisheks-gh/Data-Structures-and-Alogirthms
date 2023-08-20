f7687e38800b5204e1a4c152476373af9d85bb88
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-08-20 13:32:20 +0530

    Added SelectionSort.java
    Implementation of Selection Sort sorting algorithm.

---
+++ src/com/dsa/sorting/selectionSort/SelectionSort.java
@@ -0,0 +1,47 @@
+/** Selection sort
+ * Time complexity:
+    - Worst case: O(n^2)
+    - Best case: O(n^2)
+ * Space complexity: O(1)
+ * Stable or not?: No  */
+
+package com.dsa.sorting.selectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        SelectionSort sorter = new SelectionSort();
+        int[] arr = {4, 5, 1, 2, 3};
+        System.out.println("Array before sorting: " + Arrays.toString(arr));
+        sorter.selectionSort(arr);
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
+    }
+
+    private void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getIndexContainingMaxElementInArray(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    private int getIndexContainingMaxElementInArray(int[] arr, int start, int end) {
+        int indexContainingMax = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[indexContainingMax]) {
+                indexContainingMax = i;
+            }
+        }
+
+        return indexContainingMax;
+    }
+
+    private void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
