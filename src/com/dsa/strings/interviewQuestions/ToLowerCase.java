957ec790cd0d2fca2028e5867611dbfa871e1523
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-09-09 09:01:24 +0530

    Add ToLowerCase.java
    Problem link: https://leetcode.com/problems/to-lower-case/

---
+++ src/com/dsa/strings/interviewQuestions/ToLowerCase.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/to-lower-case/
+// Amazon (2)
+
+package com.dsa.strings.interviewQuestions;
+
+public class ToLowerCase {
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
