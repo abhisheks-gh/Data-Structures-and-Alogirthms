17b9a88207f4fcb5b7217c1582e67842d2cc1abc
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-10-08 10:41:31 +0530

    Update SubClass.java

--- src/com/oops/accessControl/SubClass.java
+++ src/com/oops/accessControl/SubClass.java
@@ -9,5 +9,9 @@ public class SubClass extends A {
     public static void main(String[] args) {
         SubClass obj = new SubClass(45, "Abhishek Kumar");
         int n = obj.num;
+
+        System.out.println(obj instanceof A); // true
+        System.out.println(obj instanceof SubClass); // true
+        System.out.println(obj instanceof Object); // true
     }
 }
