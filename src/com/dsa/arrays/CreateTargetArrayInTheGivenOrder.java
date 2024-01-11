1b73b7e56252605578a092374e0d43463330fb03
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2024-01-11 19:47:12 +0530

    Added RunningSum, CreateTargetArrInOrder, TwoSum
    Problem links - 
    Two Sum: https://leetcode.com/problems/two-sum/
    Running Sum Of 1D Array: https://leetcode.com/problems/running-sum-of-1d-array/
    Create Target Array in Given Order: https://leetcode.com/problems/create-target-array-in-the-given-order/

---
+++ src/com/dsa/arrays/CreateTargetArrayInTheGivenOrder.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+package com.assignments.arrays;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class CreateTargetArrayInTheGivenOrder {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        System.out.println( Arrays.toString(createTargetArray(nums, index)) );
+    }
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> list = new ArrayList<>(nums.length);
+        int[] target = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            target[i] = list.get(i);
+        }
+
+        return target;
+    }
+}
---
+++ src/com/dsa/arrays/TwoSum.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/two-sum/
+
+package com.assignments.arrays;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        System.out.println( Arrays.toString(twoSum(nums, 9)) );
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+ 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
